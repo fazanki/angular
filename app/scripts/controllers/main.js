642d9aad43ecf2367300396b42c7ab616a089e64
Franco Zanki <Fracno.Zanki@moneyadviceservie.org.uk>
2014-05-08 11:33:09 +0100

    adding the first angular script

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,12 @@ angular.module('learningAngularApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.teamC = [
+      {name:"Gareth", position:"back end developer" },
+      {name:"Aman", position:"qa" },
+      {name:"Karl", position:"ux" },
+      {name:"Franco", position:"front end develper" },
+      {name:"Jaky", position:"product owner" }
+    ];
   });
